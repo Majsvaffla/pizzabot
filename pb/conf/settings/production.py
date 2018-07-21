63e037510584740b65b366a2940b4b0f160c13e4
David Svenson <davidsvenson@outlook.com>
2018-07-21 11:43:31 +0200

    Report errors to Rollbar

--- pb/conf/settings/production.py
+++ pb/conf/settings/production.py
@@ -17,3 +17,21 @@ SLACK_CLIENT_SECRET = os.environ.get('SLACK_CLIENT_SECRET')
 SLACK_VERIFICATION_TOKEN = os.environ.get('SLACK_VERIFICATION_TOKEN')
 
 SLACK_BOT_USER_TOKEN = os.environ.get('SLACK_BOT_USER_TOKEN')
+
+
+# Rollbar
+
+ROLLBAR_ACCESS_TOKEN = os.environ.get("ROLLBAR_ACCESS_TOKEN")
+
+if ROLLBAR_ACCESS_TOKEN:
+    ROLLBAR = {
+        'access_token': ROLLBAR_ACCESS_TOKEN,
+        'environment': 'production',
+        'branch': 'master',
+        'root': PROJECT_ROOT,
+    }
+
+    MIDDLEWARE += ['rollbar.contrib.django.middleware.RollbarNotifierMiddlewareExcluding404']
+else:
+    from django.core.exceptions import ImproperlyConfigured
+    raise ImproperlyConfigured("'ROLLBAR_ACCESS_TOKEN' must be set")
