0e6f99cf14da44b62a1554cb86b0ce58f0ea0443
David Svenson <davidsvenson@outlook.com>
2018-07-22 18:28:21 +0200

    Use two gunicorn workers

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-exec pipenv run gunicorn pb.conf.wsgi --bind unix:/home/pizzabot/pizza.socket --access-logfile /home/pizzabot/pizza.log
+exec pipenv run gunicorn pb.conf.wsgi --bind unix:/home/pizzabot/pizza.socket --access-logfile /home/pizzabot/pizza.log --workers 2
