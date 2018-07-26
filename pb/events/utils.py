2b37464cf4089a11c26b7aadc587346a97763af7
David Svenson <davidsvenson@outlook.com>
2018-07-26 21:15:41 +0200

    Decorate API more

--- pb/events/utils.py
+++ pb/events/utils.py
@@ -6,14 +6,36 @@ from django.conf import settings
 from pb.slack.api.calls import channel_info
 
 
-def message_in_channel(channel_id, match_channel):
+def in_channel(channel_id, match_channel):
     response = channel_info(channel=channel_id)
-    
+
     if response.get('ok'):
         channel_name = response['channel']['name']
         return channel_name == match_channel
 
     return False
+
+
+def message_in_channel(channel_name):    
+    def wrapper(view_method):
+        def decorated_view(view, request, *args, **kwargs)
+            if 'event' in request.data:
+                event = request.data.get('event')
+
+                if event.get('subtype') == 'bot_message':
+                    return Response(status=status.HTTP_200_OK)
+
+                channel = event.get('channel')
+
+                if event.get('type') == 'message' and in_channel(channel, channel_name):
+                    text = event.get('text')
+                    return view_method(view, request, text, *args, **kwargs)
+
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+        return decorated_view
+
+    return wrapper
 
 
 def require_verification_token(view_method):
--- pb/events/views.py
+++ pb/events/views.py
@@ -17,25 +17,14 @@ from .utils import (
 class Events(APIView):
     @require_verification_token
     @url_verification
-    def post(self, request, *args, **kwargs):
-        if 'event' in request.data:
-            event = request.data.get('event')
+    @message_in_channel(PIZZA_CHANNEL)
+    def post(self, request, message):
+        phrase = phrases.from_text(message)
 
-            if event.get('subtype') == 'bot_message':
-                return Response(status=status.HTTP_200_OK)
+        if phrase is not None:
+            response = phrase.response
 
-            channel = event.get('channel')
+            if response is not None and response.cool:
+                post_message(text=response.text)
 
-            if event.get('type') == 'message' and message_in_channel(channel, PIZZA_CHANNEL):
-                text = event.get('text')
-                phrase = phrases.from_text(text)
-
-                if phrase is not None:
-                    response = phrase.response
-
-                    if response is not None and response.cool:
-                        post_message(text=response.text)
-
-            return Response(status=status.HTTP_200_OK)
-
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_200_OK)
