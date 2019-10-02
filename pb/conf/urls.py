6180a4d4167634b6ef6cafaa8fbba01917a67d49
David Svenson <davidsvenson@outlook.com>
2019-10-02 18:11:02 +0200

    Add health check end point

--- pb/conf/urls.py
+++ pb/conf/urls.py
@@ -1,12 +1,18 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 
 from pb.events.views import Events
 
 
+def health_check(_):
+    return HttpResponse("Tackar som frågar!")
+
+
 urlpatterns = [
+    path('-/', health_check),
     path('admin/', admin.site.urls),
     path('events/', Events.as_view()),
     *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
